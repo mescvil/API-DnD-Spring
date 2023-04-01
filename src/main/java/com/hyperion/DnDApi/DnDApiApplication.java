dbc2313437943f38b48631748e8a717ff227f814
Miguel Escoz Vilches <kyrovilches@gmail.com>
2023-04-01 19:14:04 +0200

    Commit inicial

---
+++ src/main/java/com/hyperion/DnDApi/DnDApiApplication.java
@@ -0,0 +1,13 @@
+package com.hyperion.DnDApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperion/DnDApi/DnDApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperion.DnDApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
