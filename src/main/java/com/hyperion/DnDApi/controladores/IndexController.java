0446011ea05490de82d139d1a525a244ac07ef1e
Miguel Escoz Vilches <kyrovilches@gmail.com>
2023-04-03 13:48:39 +0200

    Pruebas con los atributos del modelo

---
+++ src/main/java/com/hyperion/DnDApi/controladores/IndexController.java
@@ -0,0 +1,25 @@
+package com.hyperion.DnDApi.controladores;
+
+import com.hyperion.DnDApi.entidades.Enemigo;
+import com.hyperion.DnDApi.servicios.CriaturasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @Autowired
+    private CriaturasService servicio;
+
+    @GetMapping
+    public String cargaInicio(Model model) {
+        List<Enemigo> enemigos = servicio.obtenerEnemigos();
+        model.addAttribute("enemigos", enemigos);
+        return "index";
+    }
+}
