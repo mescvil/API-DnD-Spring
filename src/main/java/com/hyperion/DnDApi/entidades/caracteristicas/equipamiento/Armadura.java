608ea8fe3bab7aa70a6428eef82595afd5ddc094
RafaelRusRuiz <rusruizrafael@outlook.es>
2023-04-23 11:34:13 +0200

    [BugFix]Cambio de claseArmadura de int to String

--- src/main/java/com/hyperion/DnDApi/entidades/caracteristicas/equipamiento/Armadura.java
+++ src/main/java/com/hyperion/DnDApi/entidades/caracteristicas/equipamiento/Armadura.java
@@ -24,7 +24,7 @@ public class Armadura {
     private int peso;
 
     @Column(name = "clase_armadura")
-    private int claseArmadura;
+    private String claseArmadura;
 
     private String precio;
 
