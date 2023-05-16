68b746d451bb8cb5e74f75c7c8be4e92e308557e
Miguel Escoz Vilches <kyrovilches@gmail.com>
2023-05-16 10:42:25 +0200

    Servicio de los trasfondos actualizado

--- src/main/java/com/hyperion/DnDApi/repositorios/caracteristicas/trasfondos/IdiomasRepository.java
+++ src/main/java/com/hyperion/DnDApi/repositorios/caracteristicas/trasfondos/IdiomasRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IdiomasRepository extends JpaRepository<Idioma,String> {
+    Idioma findByNombre(String nombre);
 }
