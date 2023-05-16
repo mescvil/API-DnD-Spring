79289350d5c58af2d8f2e1ca038a8d78d0bda7a5
Miguel Escoz Vilches <kyrovilches@gmail.com>
2023-05-16 10:23:15 +0200

    Servicio de las competencias actualizado

--- src/main/java/com/hyperion/DnDApi/repositorios/caracteristicas/competencias/CompetenciasRepository.java
+++ src/main/java/com/hyperion/DnDApi/repositorios/caracteristicas/competencias/CompetenciasRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompetenciasRepository extends JpaRepository<Competencia,String> {
+    Competencia findByNombre(String nombre);
 }
